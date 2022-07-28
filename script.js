63a85ac4e4cc49c8a830408aa7c96e1494fe8965
Joy <uchechukwujoy21@gmail.com>
2022-07-28 09:26:41 +0100

    fitness test

---
+++ script.js
@@ -0,0 +1,46 @@
+const change = document.getElementById ("image1");
+change.addEventListener("click", (Event) => {
+    Event.target.style.color = "white";
+    Event.target.style.background = "red";
+});
+
+const par3 =document.getElementById("image2");
+par3.addEventListener("click", (event) => {
+    event.target.style.background ="blue"
+    event.target.style.color="white"
+});
+
+const paragraph =document.getElementById("image3");
+paragraph.addEventListener("click", (event) =>{
+    event.target.style.background="aqua"  
+    event.target.style.color="white"
+});
+const par4 =document.getElementById("image4");
+par4.addEventListener("click", (event) => {
+    event.target.style.background ="blueviolet"
+    event.target.style.color="white"
+});
+
+const par5 =document.getElementById("image5");
+par5.addEventListener("click", (event) => {
+    event.target.style.background ="aquamarine"
+    event.target.style.color="white"
+});
+
+const par6 =document.getElementById("image6");
+par6.addEventListener("click", (event) => {
+    event.target.style.background ="burlywood"
+    event.target.style.color="white"
+});
+
+const par7 =document.getElementById("image7");
+par7.addEventListener("click", (event) => {
+    event.target.style.background ="brown"
+    event.target.style.color="white"
+});
+
+const par8 =document.getElementById("image8");
+par8.addEventListener("click", (event) => {
+    event.target.style.background ="indianred"
+    event.target.style.color="white"
+});
